7edc44936d585b0521d65dabecd9400af62b36c3
Paul Coppolino <hta.infinite@gmail.com>
2025-03-11 10:33:33 -0400

    QoL changes

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
+import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		
-		Wizard w = new Wizard();
 		int failsafe = 0;
+		Scanner scanner = new Scanner(System.in);
 		
-		while(failsafe < 10){
-		    w.playerAttack();
+		System.out.println("START GAME\n-------------------");
+		System.out.print("Please input a name for your wizard : ");
+		String name = scanner.nextLine(); //Player inputs a name for their wizard.
+		
+		Wizard player = new Wizard(name, 100, 40); //player's wizard
+		Wizard enemy = new Wizard();
+		
+		System.out.println("-------------------\nYour enemy is " + enemy.getName() + ". Good luck, " + name + "\n-------------------");
+		
+		while(failsafe < 4){
+		    player.playerAttack();
+		    enemy.enemyAttack();
 		    failsafe+=1;
 		}
 	}
