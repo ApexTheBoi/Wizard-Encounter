3972eccdfadf85161ec311f09b538086fd18684b
Paul Coppolino <hta.infinite@gmail.com>
2025-03-11 09:16:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ public class Main
 	public static void main(String[] args) {
 		
 		Wizard w = new Wizard();
-		w.playerAttack();
+		int failsafe = 0;
+		
+		while(failsafe < 10){
+		    w.playerAttack();
+		    failsafe+=1;
+		}
 	}
 }
