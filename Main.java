5ada175e58c0692fb6c132eefd12bcf4081faa76
Paul Coppolino <hta.infinite@gmail.com>
2025-03-11 12:02:19 -0400

    Added a printInfo() method

--- Main.java
+++ Main.java
@@ -10,14 +10,15 @@ public class Main
 		String name = scanner.nextLine(); //Player inputs a name for their wizard.
 		
 		Wizard player = new Wizard(name, 100, 40); //player's wizard
-		Wizard enemy = new Wizard();
+		final Wizard enemy = new Wizard();
 		
 		System.out.println("-------------------\nYour enemy is " + enemy.getName() + ". Good luck, " + name + "\n-------------------");
 		
 		while(failsafe < 4){
-		    player.playerAttack();
-		    enemy.enemyAttack();
+		    player.turn(enemy); //player's turn. Passes in the target
+		    enemy.turn(player); //enemy's turn. Passes in the target
 		    failsafe+=1;
+		    player.printInfo(enemy); //to get the information.
 		}
 	}
 }
