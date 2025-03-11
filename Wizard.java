70deed2aa1ca86692dd09bbd96f1d9f3a220272c
Paul Coppolino <hta.infinite@gmail.com>
2025-03-11 09:16:25 -0400

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -7,8 +7,9 @@ public class Wizard{
     private boolean isBurned;
     private boolean isFrostbitten;
     private boolean isThunderstruck;
-    
-    private playerHasActed;
+    private Scanner input;
+        
+    private boolean playerHasActed;
     
     
     public Wizard(){
@@ -17,7 +18,6 @@ public class Wizard{
         isBurned = false;
         isFrostbitten = false;
         isThunderstruck = false;
-        Scanner input = new Scanner(System.in);
         playerHasActed = false;
     }
 
@@ -52,29 +52,56 @@ public class Wizard{
     }
     
     public void playerAttack(){
-        while(PlayerHasActed == false){
-        System.out.print("Please choose an attack:\n");
-        System.out.println("\t1 - Fireball (4 mp) *Inflicts burning.");
-        System.out.println("\t2 - Blizzard (8 mp) *Inflicts frostbite.");
-        System.out.println("\t3 - Thunderstrike (14 mp) *Has a 40% chance to inflict Thunderstruck.");
-        System.out.println("\t0 - Information on Statuses.");
-        choice = input.nextInt()
-        if(choice!=0){
+        
+        input = new Scanner(System.in); //creating a scanner for the user input.
+        
+        while(playerHasActed == false){ //If they haven't acted yet, ask them to.
+            System.out.print("Please choose an attack:\n");
+            System.out.println("\t1 - Fireball (4 mp) *Inflicts burning.");
+            System.out.println("\t2 - Blizzard (8 mp) *Inflicts frostbite.");
+            System.out.println("\t3 - Thunderstrike (14 mp) *Has a 40% chance to inflict Thunderstruck.");
+            System.out.println("\t0 - Information on Statuses.");
+            int choice = input.nextInt(); //Getting the user input.
+            if(choice==1){
+                fireball();
+                playerHasActed = true;
+            }
             
-        }
+            else if(choice == 2){
+                blizzard();
+                playerHasActed = true;
+            }
+            
+            else if(choice == 3){
+                thunderstrike();
+                playerHasActed = true;
+            }
+            
+            else if(choice ==0){
+                getStatusInfo();
+            }
         
         }
+        playerHasActed = false;
     }
     
-    public void fireball(Wizard target){
-        target.    
+    public void getStatusInfo(){
+        System.out.println("-----------------------------\n-----------------------------");
+        System.out.println("Burning - Flames sear the flesh of the afflicted, dealing 4 damage for the next 2 turns.");
+        System.out.println("Frostbite - Frost makes the flesh of the afflicted brittle. The next attack taken will deal double damage.");
+        System.out.println("Thunderstruck - Thunder renders the body inactive. The next turn of the afflicted will be skipped.");
+        System.out.println("-----------------------------\n-----------------------------");
     }
     
-    public void Blizzard(){
-        
+    public void fireball(){
+        System.out.println("Wizard casts fireball!");
     }
     
-    public void thunderStrike(){
-        
+    public void blizzard(){
+        System.out.println("Wizard casts blizzard!");
+    }
+    
+    public void thunderstrike(){
+        System.out.println("Wizard casts thunderstrike!");
     }
 }
